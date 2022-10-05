fd68d31cee0a9051c2e1a30d9c21fef1d53be4e1
Mateo Chavez <113551958+mat212121@users.noreply.github.com>
2022-10-05 19:43:09 +0000

    ranges

--- loops.py
+++ loops.py
@@ -115,31 +115,37 @@ def loop():
   # For Loops Practice #2
   # Given the following list of numbers, calculate the sum of all the numbers using For loops and store the result of the sum in a variable called sum_numbers:
   
-  list_numbers = [1,5,8,7,6,8,2,5,2,6,4,8,5,9,8,3,5,4,2,5,6,4]
-  sum_numbers = 0 
+#   list_numbers = [1,5,8,7,6,8,2,5,2,6,4,8,5,9,8,3,5,4,2,5,6,4]
   for list_numbers in list_numbers:
-    sum_numbers = sum_numbers +list_numbers
-    print(f" sum of list numbers is {sum_numbers}")
+     sum_numbers = 0
+     sum_numbers = sum_numbers +list_numbers
+     print(f" sum of list numbers is {sum_numbers}")
   
   
   
   
-  # For Loops Practice #3
-  # Given the following list of numbers, perform the sum of all even and odd* numbers separately in the variables sum_even and sum_odd respectively:
+#   # For Loops Practice #3
+#   # Given the following list of numbers, perform the sum of all even and odd* numbers separately in the variables sum_even and sum_odd respectively:
   
-  list_numbers = [1,5,8,7,6,8,2,5,2,6,4,8,5,9,8,3,5,4,2,5,6,4]
+#   list_numbers = [1,5,8,7,6,8,2,5,2,6,4,8,5,9,8,3,5,4,2,5,6,4]
   
-  # *Recall from previous days: the modulus (or remainder) of a number divided by 2 is zero when said value is even, and 1 when it is odd
+#   # *Recall from previous days: the modulus (or remainder) of a number divided by 2 is zero when said value is even, and 1 when it is odd
   
-  # num % 2 == 0 (even values)
+#   # num % 2 == 0 (even values)
   
-  # num % 2 == 1 (odd values)
+#   # num % 2 == 1 (odd values)
   
-  list_numbers = [1,5,8,7,6,8,2,5,2,6,4,8,5,9,8,3,5,4,2,5,6,4]
+#   list_numbers = [1,5,8,7,6,8,2,5,2,6,4,8,5,9,8,3,5,4,2,5,6,4]
   
-  # sum_even = 
+#   sum_even = 0
   
-  # sum_odd = 
+#   sum_odd = 0 
 
-
-
+# for num in list_numbers:
+#   if num % 2 == 1:
+#     sum_even = sum_even + num
+#   elif num % 2 == 1:
+#     sum_odd == sum_odd + num
+    
+# print(f"evens are {sum_even}")
+# print(f"odds are {sum_odd}")
